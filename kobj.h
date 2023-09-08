93cc680dd194752de173c2ad7c0eb19888876b10
krisvers <105808514+krisvers@users.noreply.github.com>
2023-09-08 09:15:27 -0400

    Update kobj.h

--- kobj.h
+++ kobj.h
@@ -15,6 +15,10 @@ typedef enum KObjSuccessEnum {
 /*
   kobj_parse():
 
+  Return values (reference KObjSuccessEnum/kobj_success_enum):
+    anything negative is an error,
+    everything else is success
+
   Required arguments:
     void * buffer (a pointer to a buffer which you would like to parse)
     unsigned long long int length (length of the buffer you'd like to parse)
@@ -22,7 +26,13 @@ typedef enum KObjSuccessEnum {
   Optional output arguments:
     unsigned int * out_flags (a pointer to an unsigned int that holds flag values)
     float * out_vertices (a pointer to an *already* allocated buffer where the vertices will be stored)
-    unsigned long long int * out_vertices_length (a pointer to a ULL where the length of the vertices array will be stored)
+    unsigned long long int * out_vertices_length (a pointer to an unsigned long long int where the length of the vertices array will be stored)
+    unsigned int * out_indices (a pointer to an *already* allocated buffer where the indices/faces will be stored)
+    unsigned long long int * out_indices_length (a pointer to an unsigned long long int where the length of the indices/faces array will be stored)
+    float * out_normals (a pointer to an *already* allocated buffer where the normals will be stored)
+    unsigned long long int * out_normals_length (a pointer to an unsigned long long int where the length of the normals array will be stored)
+    float * out_uv (a pointer to an *already* allocated buffer where the texture UVs will be stored)
+    unsigned long long int * out_uv_length (a pointer to an unsigned long long int where the length of the UV array will be stored)
 */
 kobj_success_enum kobj_parse(void * buffer, unsigned long long int length, unsigned int * out_flags, float * out_vertices, unsigned long long int * out_vertices_length, unsigned int * out_indices, unsigned long long int * out_indices_length, float * out_normals, unsigned long long int * out_normals_length, float * out_uv, unsigned long long int * out_uv_length);
 
