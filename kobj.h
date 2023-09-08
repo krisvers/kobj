0921bc831529472673c85c83631f6807a15a49e3
krisvers <105808514+krisvers@users.noreply.github.com>
2023-09-08 09:01:50 -0400

    Update kobj.h

--- kobj.h
+++ kobj.h
@@ -12,6 +12,18 @@ typedef enum KObjSuccessEnum {
 	KOBJ_SUCCESS = 0,
 } kobj_success_enum;
 
+/*
+  kobj_parse():
+
+  Required arguments:
+    void * buffer (a pointer to a buffer which you would like to parse)
+    unsigned long long int length (length of the buffer you'd like to parse)
+
+  Optional output arguments:
+    unsigned int * out_flags (a pointer to an unsigned int that holds flag values)
+    float * out_vertices (a pointer to an *already* allocated buffer where the vertices will be stored)
+    unsigned long long int * out_vertices_length (a pointer to a ULL where the length of the vertices array will be stored)
+*/
 kobj_success_enum kobj_parse(void * buffer, unsigned long long int length, unsigned int * out_flags, float * out_vertices, unsigned long long int * out_vertices_length, unsigned int * out_indices, unsigned long long int * out_indices_length, float * out_normals, unsigned long long int * out_normals_length, float * out_uv, unsigned long long int * out_uv_length);
 
 #endif
